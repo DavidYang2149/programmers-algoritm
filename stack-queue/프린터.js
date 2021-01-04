a29060f5cfcd322737c3a050e78cabc33c3ae00f
davidyang2149 <davidyang2149@gmail.com>
2021-01-04 21:56:22 +0900

    Add 프린터.js(Init)
    Programmers Algoritm.
    - Add 프린터(Initialize).

---
+++ stack-queue/프린터.js
@@ -0,0 +1,16 @@
+/*
+  요구사항
+1. 프린터는 중요도가 높은 순으로 인쇄한다.
+2. 
+
+  필요기술
+1. 
+*/
+function solution(priorities, location) {
+  var answer = 0;
+  return answer;
+}
+
+// node stack-queue/프린터.js
+// console.log(solution([2, 1, 3, 2], 2)); // 1
+// console.log(solution([1, 1, 9, 1, 1, 1], 0));  // 5
