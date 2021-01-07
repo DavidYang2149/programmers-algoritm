aa854176b095bfe4e20d192868925279e972c430
davidyang2149 <davidyang2149@gmail.com>
2021-01-07 10:16:49 +0900

    Add 가장-큰-수.js(Init)
    Programmers Algoritm.
    - Add sort 가장 큰 수(Initialize).

---
+++ sort/가장-큰-수.js
@@ -0,0 +1,19 @@
+/*
+  요구사항
+1. 0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 찾아라.
+
+  필요기술
+1. 배열의 사용법
+
+  후기
+1. 시간 제한: 2시간 / 사용 시간:
+*/
+
+function solution(numbers) {
+  var answer = '';
+  return answer;
+}
+
+// node sort/가장-큰-수.js
+// console.log(solution([6, 10, 2])); // "6210"
+// console.log(solution([3, 30, 34, 5, 9])); // "9534330"
